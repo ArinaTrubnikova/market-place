fce0b7986d8000f42c749a658b5252f459486521
Arina Trubnikova <atrubnikova@bivgroup.ru>
2025-12-09 17:13:58 +0300

    new

--- src/app/basket-layout/basket/purchase-history/purchase-history.ts
+++ src/app/basket-layout/basket/purchase-history/purchase-history.ts
@@ -5,17 +5,16 @@ import { combineLatest, map, Observable } from 'rxjs';
 import { AsyncPipe, CurrencyPipe } from '@angular/common';
 import { GetProductService } from '../../../product-component/services/get-product.service';
 import { Card } from '../../../product-component/interfaces/product-card.model';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'purchase-history',
-  imports: [AsyncPipe, CurrencyPipe],
+  imports: [AsyncPipe, CurrencyPipe, MatCardModule],
   templateUrl: './purchase-history.html',
   providers: [],
-  styleUrl: './purchase-history.scss'
+  styleUrl: './purchase-history.scss',
 })
-
 export class PurchaseHistoryComponent {
-
   private dataService: DataService = inject(DataService);
   private getProductService: GetProductService = inject(GetProductService);
   public purchaseHistoryCard$!: Observable<any>;
@@ -24,49 +23,53 @@ export class PurchaseHistoryComponent {
   ngOnInit() {
     this.purchaseHistoryCard$ = combineLatest([
       this.dataService.historyPurchase$,
-      this.getProductService.getProduct()
-    ])
-      .pipe(
-        map(([productsBuy, productsCard]: [BuyProduct[], Card[]]) => {
+      this.getProductService.getProduct(),
+    ]).pipe(
+      map(([productsBuy, productsCard]: [BuyProduct[], Card[]]) => {
+        const cardsProduct = new Map();
+        productsCard.forEach((card) => {
+          const { id, ...cardInfo } = card;
+          cardsProduct.set(id, { ...card });
+        });
 
-          const cardsProduct = new Map();
-          productsCard.forEach(card => {
-            const { id, ...cardInfo } = card;
-            cardsProduct.set(id, { ...card });
-          });
+        this.totalPrice = productsBuy.reduce(
+          (total, buy) =>
+            total +
+            buy.productData.reduce((sum, product) => {
+              const card = productsCard.find((card) => card.id === product.id);
+              return sum + product.count * (card?.cost || 0);
+            }, 0),
+          0
+        );
 
-          this.totalPrice = productsBuy.reduce((total, buy) =>
-            total + buy.productData.reduce((sum, product) => {
-              const card = productsCard.find(card => card.id === product.id);
-              return sum + (product.count * (card?.cost || 0));
-            }, 0)
-            , 0);
-
-
-          return productsBuy.map(buy => {
-            const products = buy.productData.map(product => {
-              return {
-                ...cardsProduct.get(product.id),
-                count: product.count
-              }
-            });
-
+        return productsBuy.map((buy) => {
+          const products = buy.productData.map((product) => {
             return {
-              ...buy,
-              productData: products
-            }
+              ...cardsProduct.get(product.id),
+              count: product.count,
+            };
           });
-        })
-      )
+
+          return {
+            ...buy,
+            productData: products,
+          };
+        });
+      })
+    );
     console.log(this.purchaseHistoryCard$);
   }
 
   getTotalPrice() {
-    const productsBuy = this.purchaseHistoryCard$.pipe(map((buy: BuyProduct) => buy.productData
-      .reduce((total: number, product: any) =>
-        total + (product.count * product.cost), 0)
-    ));
+    const productsBuy = this.purchaseHistoryCard$.pipe(
+      map((buy: BuyProduct) =>
+        buy.productData.reduce(
+          (total: number, product: any) => total + product.count * product.cost,
+          0
+        )
+      )
+    );
 
-    return console.log(productsBuy)
+    return console.log(productsBuy);
   }
 }
--- src/app/common/date-validator.ts
+++ src/app/common/date-validator.ts
@@ -1,41 +1,39 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export function dateValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const inputDate = control.value;
 
-        const inputDate = control.value;
-        // console.log('Validating:', inputDate, 'Type:', typeof inputDate);
-
-        if (!inputDate) {            
-            return null;
-        }
-        inputDate.setHours(0, 0, 0, 0);
-        const today = new Date();
-        today.setHours(0, 0, 0, 0);
-        const minAgeDate = new Date();
-        minAgeDate.setFullYear(today.getFullYear() - 18);
-        const maxAgeDate = new Date();
-        maxAgeDate.setFullYear(today.getFullYear() - 75);
-
-        if (inputDate > today) {
-            return {
-                futureDate: {
-                    value: inputDate
-                }
-            };
-        } else if (inputDate > minAgeDate) {
-            return {
-                minAge: {
-                    value: inputDate
-                }
-            };
-        } else if (inputDate < maxAgeDate) {
-            return {
-                maxAge: {
-                    value: inputDate
-                }
-            };
-        }
-        return null;
+    if (!inputDate) {
+      return null;
     }
+    inputDate.setHours(0, 0, 0, 0);
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    const minAgeDate = new Date();
+    minAgeDate.setFullYear(today.getFullYear() - 18);
+    const maxAgeDate = new Date();
+    maxAgeDate.setFullYear(today.getFullYear() - 75);
+
+    if (inputDate > today) {
+      return {
+        futureDate: {
+          value: inputDate,
+        },
+      };
+    } else if (inputDate > minAgeDate) {
+      return {
+        minAge: {
+          value: inputDate,
+        },
+      };
+    } else if (inputDate < maxAgeDate) {
+      return {
+        maxAge: {
+          value: inputDate,
+        },
+      };
+    }
+    return null;
+  };
 }

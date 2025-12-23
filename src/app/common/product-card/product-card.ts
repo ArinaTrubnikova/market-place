c170c2a3daede0b9ae8a761d8747a3713015f9cc
Arina Trubnikova <atrubnikova@bivgroup.ru>
2025-12-23 13:41:02 +0300

    new commn component

--- src/app/common/product-card/product-card.ts
+++ src/app/common/product-card/product-card.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, input, output } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { StorageService } from '../../services/storage.service';
+import { GetProductService } from '../../product-component/services/get-product.service';
+import { Card } from '../../product-component/interfaces/product-card.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'product-card',
   templateUrl: 'product-card.html',
   styleUrl: 'product-card.scss',
+  imports: [MatCardModule, CurrencyPipe],
 })
-export class ProductCardComponent {}
+export class ProductCardComponent {
+  @Input() products: Card[] = [];
+  isModalShow = output<boolean>();
+
+  constructor() {}
+
+  showModal(elem: boolean) {
+    this.isModalShow.emit(elem);
+  }
+}
